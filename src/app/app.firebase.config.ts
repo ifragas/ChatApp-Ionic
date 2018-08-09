c1f9b883a4c49e210aba1540c13ee9f681488013
Ian Fraga <ianfraga92@gmail.com>
2018-08-09 16:15:11 -0300

    Update app.firebase.config.ts

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,8 +1,3 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBqF3I_YbJlOT2z7EwmaKDe_Q66OKk-QhU",
-    authDomain: "chat-ionic-66eca.firebaseapp.com",
-    databaseURL: "https://chat-ionic-66eca.firebaseio.com",
-    projectId: "chat-ionic-66eca",
-    storageBucket: "chat-ionic-66eca.appspot.com",
-    messagingSenderId: "266746782875"
+    
   };
